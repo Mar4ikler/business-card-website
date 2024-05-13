1f8e421ceb1393cfae1fbd7036fb9c974015e8ef
Mark Levsha <marklevsha4@gmail.com>
2024-05-13 17:32:58 +0300

    add fonts

--- website/src/App.js
+++ website/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import MainView from './views/MainView/MainView';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 function App() {
     return (

3efd306e922e6301ba29476fa4963d681e3ecd44
Марк Левша <mediocrity@Marks-MacBook-Pro.local>
2024-08-04 21:44:17 +0300

    change router

--- website/src/App.js
+++ website/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import MainView from './views/MainView/MainView';
 import 'bootstrap/dist/css/bootstrap.min.css';
